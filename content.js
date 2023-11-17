32ce49b37007a451b0d8bcdc3f7fba8bcdb64308
Romain Vie <romain.vie@gmail.com>
2023-11-17 09:06:23 +0100

    Parsing des hooks et du titre du scénario Affichage de la chaine résultat dans la console

--- content.js
+++ content.js
@@ -12,6 +12,32 @@ function isTagFoundInParents(el, type, tag) {
 	return false;	
 }
 
+function parseScenario(node) {
+	let textContent = '';
+	
+	if (node.nodeName.toLowerCase() === "table" || (node.classList && (node.classList.contains("scenario-tags") || node.classList.contains("scenarioTitle")))){
+		for (let childNode of node.childNodes) {
+			textContent += parseScenario(childNode);
+		}
+		textContent += "\n";
+	}
+	else if (!(node.classList && (node.classList[0] || []).includes("angular-FeatureViewer"))) {
+		
+		// Check if the node has text content
+		if (node.nodeType === Node.TEXT_NODE) {
+			textContent += node.textContent + " ";
+		}
+
+		// Iterate over child nodes
+		for (let childNode of node.childNodes) {
+			textContent += parseScenario(childNode);
+		}
+	}
+	
+	return textContent;	
+}
+
+
 // Retrieve the clicked element when the context menu is displayed
 document.addEventListener("contextmenu", function(event){
     clickedElt = event.target;
@@ -21,8 +47,10 @@ document.addEventListener("contextmenu", function(event){
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 		if(request == "getClickedElt") {
 			if (isTagFoundInParents(clickedElt, "div", "scenarios")){
-				//navigator.clipboard.writeText("CLIPBOARD");		
-				window.alert("Gherkin copied to clipboard.");
+				//navigator.clipboard.writeText("CLIPBOARD");	
+				const content = parseScenario(clickedElt);
+				console.log(content);
+				//window.alert("Gherkin copied to clipboard.");
 			}
 			else{
 				window.alert("This is NOT a linigdoc scenario.");
@@ -30,3 +58,4 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 		sendResponse(null);
     }
 });
+
