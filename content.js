c5a36af89dd04d092c32f1480b1af20ef13417e8
Romain Vie <romain.vie@gmail.com>
2023-11-17 11:45:22 +0100

    Gherkin example tables can now be parsed Tables in steps can be parsed as well The resulting string is still output to the console for now

--- content.js
+++ content.js
@@ -1,59 +1,198 @@
 // variable to store the clicked element (a link)
 var clickedElt = null;
 
-function isTagFoundInParents(el, type, tag) {
-	while (el.parentNode) {
-        el = el.parentNode;
-        if (el.nodeName.toLowerCase() === type && el.classList.contains( tag )) {
-			clickedElt = el;
-			return true;			
-		}
-    }
-	return false;	
-}
-
-function parseScenario(node) {
-	let textContent = '';
-	
-	if (node.nodeName.toLowerCase() === "table" || (node.classList && (node.classList.contains("scenario-tags") || node.classList.contains("scenarioTitle")))){
-		for (let childNode of node.childNodes) {
-			textContent += parseScenario(childNode);
-		}
-		textContent += "\n";
-	}
-	else if (!(node.classList && (node.classList[0] || []).includes("angular-FeatureViewer"))) {
-		
-		// Check if the node has text content
-		if (node.nodeType === Node.TEXT_NODE) {
-			textContent += node.textContent + " ";
-		}
-
-		// Iterate over child nodes
-		for (let childNode of node.childNodes) {
-			textContent += parseScenario(childNode);
-		}
-	}
-	
-	return textContent;	
-}
-
-
-// Retrieve the clicked element when the context menu is displayed
-document.addEventListener("contextmenu", function(event){
+// Event listener for the "contextmenu" event on the document
+// This event is triggered when the user right-clicks to open the context menu
+document.addEventListener("contextmenu", function(event) {
+    // Retrieve the clicked element when the context menu is displayed
     clickedElt = event.target;
 }, true);
+
+// Function to check if a specific tag with a given class is found in the ancestors of an element
+function isTagFoundInParents(el, type, tag) {
+    // Iterate through the parent nodes of the given element
+    while (el.parentNode) {
+        // Move to the parent node
+        el = el.parentNode;
+
+        // Check if the current parent node has the specified type and class
+        if (el.nodeName.toLowerCase() === type && el.classList.contains(tag)) {
+            // Set the clicked element to the current parent node
+            clickedElt = el;
+
+            // Return true, indicating that the tag with the specified class is found
+            return true;
+        }
+    }
+
+    // If the tag with the specified class is not found in any ancestor, return false
+    return false;
+}
+
+// This function removes the first two columns from each line of a table represented by a markdown-style string
+function removeFirstTwoColumns(inputString) {
+    // Split the input string into an array of lines
+    let lines = inputString.split(/\r?\n/);
+
+    // Process each line to remove the first two columns
+    let processedLines = lines.map(line => {
+        // Split the line into an array of columns using the pipe character as the separator
+        let columns = line.split('|').map(col => col.trim());
+
+        // Remove the first two columns (index 0 to 2) and join the remaining columns with the pipe character
+        let remainingColumns = columns.slice(3).join(' | ');
+
+        // Return the line with the modified columns, including the starting pipe character
+        return `| ${remainingColumns}`;
+    });
+
+    // Join the processed lines back into a single string, excluding the last (empty) line
+    let resultString = processedLines.slice(0, -1).join('\n');
+
+    // Return the final string with the first two columns removed from each line
+    return resultString;
+}
+
+// This function parses a table structure represented by an HTML node and converts it into a markdown-style table.
+function parseTable(node) {
+    // Initialize a variable to store the resulting text content
+    let textContent = '';
+
+    // Check if the current node is a table row (<tr>)
+    if (node.nodeName.toLowerCase() === "tr") {
+        // Add the starting pipe character for the beginning of a table row
+        textContent += "| ";
+
+        // Iterate over child nodes of the table row
+        for (let childNode of node.childNodes) {
+            // Recursively call the parseTable function for each child node
+            textContent += parseTable(childNode);
+        }
+
+        // Add a newline character at the end of the table row
+        textContent += "\n";
+    }
+    // Check if the current node is a table cell (<td>) or header cell (<th>)
+    else if (node.nodeName.toLowerCase() === "td" || node.nodeName.toLowerCase() === "th") {
+        // Iterate over child nodes of the table cell or header cell
+        for (let childNode of node.childNodes) {
+            // Recursively call the parseTable function for each child node
+            textContent += parseTable(childNode) + " | ";
+        }
+    }
+    // If the current node is neither a table row nor a table cell, it may contain text content
+    else {
+        // Check if the node has text content
+        if (node.nodeType === Node.TEXT_NODE) {
+            // Append the text content followed by a space
+            textContent += node.textContent + " ";
+        }
+
+        // Iterate over child nodes of the current node
+        for (let childNode of node.childNodes) {
+            // Recursively call the parseTable function for each child node
+            textContent += parseTable(childNode);
+        }
+    }
+
+    // Return the accumulated text content
+    return textContent;
+}
+
+// This function parses a scenario represented by an HTML node and converts it into a Gherkin-style text.
+function parseScenario(node) {
+    // Initialize a variable to store the resulting text content
+    let textContent = '';
+
+    // Check if the current node is a table row (<tr>) or has certain classes indicating scenario-related content
+    if (
+        node.nodeName.toLowerCase() === "tr" ||
+        (node.classList &&
+            (node.classList.contains("scenario-tags") ||
+                node.classList.contains("scenarioTitle") ||
+                (node.classList[0] || []).includes("example-header")))
+    ) {
+        // Iterate over child nodes of the current node
+        for (let childNode of node.childNodes) {
+            // Recursively call the parseScenario function for each child node
+            textContent += parseScenario(childNode);
+        }
+
+        // Add a newline character if there is text content
+        if (textContent) textContent += "\n";
+    }
+    // Check if the current node represents a Gherkin table with the class "gherkin-table"
+    else if (node.classList && node.classList.contains("gherkin-table")) {
+        // Iterate over child nodes of the current node and parse each as a table
+        for (let childNode of node.childNodes) {
+            textContent += parseTable(childNode);
+        }
+
+        // Remove the first two columns from the resulting table
+        textContent = removeFirstTwoColumns(textContent);
+    }
+    // Check if the current node represents a step argument table with the class "stepArgumentTable"
+    else if (node.classList && (node.classList[0] || []).includes("stepArgumentTable")) {
+        // Add a newline character before parsing the table
+        textContent += "\n";
+
+        // Iterate over child nodes of the current node and parse each as a table
+        for (let childNode of node.childNodes) {
+            textContent += parseTable(childNode);
+        }
+    }
+    // Check if the current node has certain classes that should be ignored
+    else if (
+        node.classList &&
+        ((node.classList[0] || []).includes("dropdown__example") ||
+            (node.classList[0] || []).includes("background_expander"))
+    ) {
+        // Ignore this node for now
+    } else {
+        // Check if the node has text content
+        if (node.nodeType === Node.TEXT_NODE) {
+            // Append the text content followed by a space
+            textContent += node.textContent + " ";
+        }
+
+        // Iterate over child nodes of the current node
+        for (let childNode of node.childNodes) {
+            // Recursively call the parseScenario function for each child node
+            textContent += parseScenario(childNode);
+        }
+    }
+
+    // Return the accumulated text content
+    return textContent;
+}
+
+// This function takes a string as input and removes multiple spaces and empty lines from it.
+function removeExtraSpacesAndLines(inputString) {
+    // Remove multiple spaces and replace with a single space
+    let stringWithoutExtraSpaces = inputString.replace(/ +/g, ' ');
+
+    // Remove empty lines
+    // The regular expression /^\s*[\r\n]/gm matches any line that contains only whitespace characters
+    // (\s*), followed by either a carriage return (\r) or newline (\n) character.
+    // The 'm' flag enables multiline mode, allowing the ^ and $ to match the start and end of each line.
+    let stringWithoutEmptyLines = stringWithoutExtraSpaces.replace(/^\s*[\r\n]/gm, '');
+
+    // Return the string with multiple spaces and empty lines removed
+    return stringWithoutEmptyLines;
+}
 
 // When the menu item is clicked, replace clicked link by an image
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 		if(request == "getClickedElt") {
 			if (isTagFoundInParents(clickedElt, "div", "scenarios")){
 				//navigator.clipboard.writeText("CLIPBOARD");	
-				const content = parseScenario(clickedElt);
+				let content = parseScenario(clickedElt);
+				content = removeExtraSpacesAndLines(content);	
 				console.log(content);
 				//window.alert("Gherkin copied to clipboard.");
 			}
 			else{
-				window.alert("This is NOT a linigdoc scenario.");
+				window.alert("This is not a livingdoc scenario.");
 			}			
 		sendResponse(null);
     }
